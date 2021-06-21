878ce43829c26c98824c43def955c5f40b937043
JohanNicolasTorres <jnrt.96@ieee.org>
2021-06-20 22:32:48 -0500

    Creacion Class Player, encabezados-cod fuente

---
+++ include/Player.h
@@ -0,0 +1,17 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+
+class Player
+{
+    public:
+        Player();
+
+        void Callinput();
+
+    protected:
+
+    private:
+};
+
+#endif // PLAYER_H
---
+++ src/Player.cpp
@@ -0,0 +1,13 @@
+#include "Player.h"
+#include <iostream>
+
+using namespace std;
+
+Player::Player()
+{
+    //ctor
+}
+
+void Player::Callinput(){
+    cout << "Aqui llamaria los input";
+}
